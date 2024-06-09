23240db04e38574b02f0472a733afaa12682715a
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-06-09 16:06:18 -0300

    many-to-many association with JoinTable

--- src/main/java/com/synchrode/purchase/entities/Category.java
+++ src/main/java/com/synchrode/purchase/entities/Category.java
@@ -1,17 +1,18 @@
 package com.synchrode.purchase.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -24,7 +25,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore 
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
